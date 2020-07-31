08785090f6665a0b2dcb7d071a1a68fc6e5c2a41
Henry Ma <henrito3.cn@gmail.com>
2020-07-30 22:40:16 -0400

    Update Heaps.py

--- Heaps.py
+++ Heaps.py
@@ -2,4 +2,10 @@
 # Heap is a tree data strcture that satifies heap properties
 
 # Priority Que
-# Implements a set S of elements, each of elements associated with a key
+# Implements a set S of elements, each of the elements associated with a key
+
+# Some functions of a priority que
+# Insert(S, x): insert element x into S
+# Max(S): return element of S with the largest key
+# Extract(S): return element of S with the largest key and remove it
+# Increase Key(S, x, k): increase the value of x's key to new value k 
