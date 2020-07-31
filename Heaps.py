6e5572246946270f871f51537f4e7452da2eea10
Henry Ma <henrito3.cn@gmail.com>
2020-07-30 22:43:00 -0400

    Update Heaps.py

--- Heaps.py
+++ Heaps.py
@@ -1,6 +1,3 @@
-# Heaps
-# Heap is a tree data strcture that satifies heap properties
-
 # Priority Que
 # Implements a set S of elements, each of the elements associated with a key
 
@@ -9,3 +6,7 @@
 # Max(S): return element of S with the largest key
 # Extract(S): return element of S with the largest key and remove it
 # Increase Key(S, x, k): increase the value of x's key to new value k 
+
+# Heaps
+# An array visualized an nearly complete binary tree
+
