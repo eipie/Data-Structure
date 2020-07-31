8783027c8724db5a27fb3a18f7ddc5f050fdde1d
Henry Ma <henrito3.cn@gmail.com>
2020-07-31 06:18:13 -0400

    Update Heaps.py

--- Heaps.py
+++ Heaps.py
@@ -1,12 +1,3 @@
-# Priority Que
-# Implements a set S of elements, each of the elements associated with a key
-
-# Some functions of a priority que
-# Insert(S, x): insert element x into S
-# Max(S): return element of S with the largest key
-# Extract(S): return element of S with the largest key and remove it
-# Increase Key(S, x, k): increase the value of x's key to new value k 
-
 # Heaps
 # An array visualized an nearly complete binary tree
 
@@ -18,3 +9,18 @@
     # the key of a node is greater or equal to the keys of its children
 # Min heap property
     # the key ofa node is less or equal to the keys of its children
+
+# Some functions of a priority que
+# Insert(S, x): insert element x into S
+# Max(S): return element of S with the largest key
+# Extract(S): return element of S with the largest key and remove it
+# Increase Key(S, x, k): increase the value of x's key to new value k 
+
+# Some application of binary heaps
+# Heap Sort
+    # Heap sort sorts a binary heap in O(n*log(n))  
+# Priority Que
+    # Implements a set S of elements, each of the elements associated with a key
+# Graph Algorithm
+    # Shortest path algorithm, Minimum spanning tree
+
