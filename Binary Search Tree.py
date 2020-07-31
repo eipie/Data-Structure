cd19db7d62cb8204913ebcc2c54182c9af5e425c
Henry Ma <henrito3.cn@gmail.com>
2020-07-31 08:53:36 -0400

    Create Binary Search Tree.py

---
+++ Binary Search Tree.py
@@ -0,0 +1,10 @@
+# Binary Search Tree
+# A completely symmetrical binary tree 
+
+# Building a binary search tree
+# Every node of the binary tree is an object, which has a few attribtes:
+    # The key
+    # The data
+    # left
+    # right
+    # pointer
