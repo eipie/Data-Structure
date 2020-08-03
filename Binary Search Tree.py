54aaf41e2595fbb5151c9ffc12a205f76aa4b399
Henry Ma <henrito3.cn@gmail.com>
2020-08-03 08:04:08 +0800

    Create Binary Search Tree.py

---
+++ Binary Search Tree.py
@@ -0,0 +1,15 @@
+# Binary Search Tree
+
+# 1. Binary search tree
+    # Binary search tree is a particular type of binary tree, where in any node, 
+    # the left child of the node is always greater and the right
+    # child is always smaller than the node
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
