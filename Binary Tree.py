6204ec14406d3ea0cd3736d0374b982086d1bc93
Henry Ma <henrito3.cn@gmail.com>
2020-08-04 19:48:02 +0800

    Update Binary Tree.py

--- Binary Tree.py
+++ Binary Tree.py
@@ -1,4 +1,4 @@
-# Binary Search Tree
+# Binary Tree
 
 # Building a binary search tree
 # Every node of the binary tree is an object, which has a few attribtes:
@@ -8,31 +8,10 @@
     # right: the right child of the node, if the node does not have a right child, then the attribute will be null
     # pointer: that points to the nodes corresponding to its left child, right child and parent
 
-# Binary Search Tree Property
-    # Let x be a node in a binary search tree. If y is a node in the left subtree
-    # of x, then y.key <= x,key. If y is a node in the right subtree of x, then
-    # y.key >= x.key
-
-# Binary Search Tree Traveral
-# Inorder tree walk: prints the key of the root of a subtree between printing the values in its left subtree 
-#                    and printing those in its right subtree.
-# Preorder tree walk: prints the root before the values in either subtree
-# Postorder tree walk: prints the root after the values in its subtrees
-
-# Binary Search Tree Query
-# Search Algorithm
-    # Given the a pointer to the root of the tree and a key k, tree-search
-    # returns a pointer to a node with key k if one exists; otherwise, it returns NIL.
-    # Time Complexity: O(n)
-
-# Finding Maximum and Minimum Algorithm
-    # A pair of symmetric algorithm that starts from the root and constantly looking for the left/right child until it's null
-    # Time Complexity: O(n)
-
-# Successor and Predessor
-    # The successor of a node is the node with the smallest key greater than the node's key
-
-# Insertion and deletion
+# Binary Tree Traveral
+# Inorder tree walk: left child, root, then right child
+# Preorder tree walk: root, left child, then right child
+# Postorder tree walk: left child, right child, then root
 
 # -------------------------------------------------------------------------------------------------------------
 # Implementation
@@ -78,12 +57,9 @@ class Tree:
                 queue.append(currentNode.right)
 
 newTree = Tree()
-newTree.add(1)
-newTree.add(2)
-newTree.add(3)
-newTree.add(4)
-newTree.add(5)
-newTree.add(6)
+arr = [i for i in range (1,6)]
+for a in arr:
+    newTree.add(a)
 newTree.breadthTraveral()
 
 
