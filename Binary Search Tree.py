30c31f039c73173d00dc1e2aad759f1ab7de4b93
Henry Ma <henrito3.cn@gmail.com>
2020-08-04 19:48:00 +0800

    Update Binary Search Tree.py

--- Binary Search Tree.py
+++ Binary Search Tree.py
@@ -4,6 +4,22 @@
     # Binary search tree is a particular type of binary tree, where in any node, 
     # the left child of the node is always greater and the right
     # child is always smaller than the node
+
+# Binary Search Tree Property
+    # Let x be a node in a binary search tree. If y is a node in the left subtree
+    # of x, then y.key <= x,key. If y is a node in the right subtree of x, then
+    # y.key >= x.key
+
+# Successor and Predessor
+    # The successor of a node is the node with the smallest key greater than the node's key
+
+# 2. Binary search tree algorithms
+# Finding Maximum and Minimum Algorithm
+    # A pair of symmetric algorithm that starts from the root and constantly looking for the left/right child until it's null
+    # Time Complexity: O(n)
+# Insert a node into the binary tree algorithm
+    # 
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -41,15 +57,13 @@ class BinarySearchTree:
             self._print_tree(currentNode.left)
             print(str(currentNode.data))
             self._print_tree(currentNode.right)
-
-    def height(self):
-        if self.root is None:
-            return 0
         
 newTree = BinarySearchTree()
 arr = [33, 1, 45]
 for a in arr:
     newTree.insert(a)
+
+
 
 
 
