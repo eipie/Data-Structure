94fa316d71f9d3937c291d34c1973629dd38c6c6
Henry Ma <henrito3.cn@gmail.com>
2020-08-04 19:48:05 +0800

    Create Hash.py

---
+++ Hash.py
@@ -0,0 +1,12 @@
+# Hash 
+
+# 1. Direct Address Tables
+    # An array that represents the dynamic set of a universe of elements
+    # Each element(slot) corresponds to a key in the universe
+    # The table can perform three functions: search, insert and delete 
+
+# 2. Hash Table 
+    # Capable of storing large amount of data
+    # Uses a hash funciton h(k) to computer the slot from the key
+
+    
