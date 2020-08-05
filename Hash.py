0acb68db1a029c560c937cedfef0a93c2332813f
Henry Ma <henrito3.cn@gmail.com>
2020-08-05 09:22:07 +0800

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -7,9 +7,14 @@
 
 # 2. Hash Table 
     # Capable of storing large amount of data
-    # Uses a hash funciton h(k) to computer the slot from the key
-
+    # Uses a hash function h(k) to computer the slot from the key
+    # the average-case performance of hasing depends on how well the hash function h 
+    # distributes the set of keys to be stored among the m slots
+    
 # 3. Collision
     # When two keys hash to the same slot, the situation is called a collions
     # A "random"-looking hash function can minimize the number of collions
-    
+# 4. Collision resolution by chaining
+    # In chaining, we place all the elements that has to the same slot into the same
+    # linked list
+    # 
