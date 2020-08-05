ee9c0397cfed0a4a5570f9a5f49ec8878b4167c8
Henry Ma <henrito3.cn@gmail.com>
2020-08-05 09:13:59 +0800

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -9,4 +9,7 @@
     # Capable of storing large amount of data
     # Uses a hash funciton h(k) to computer the slot from the key
 
+# 3. Collision
+    # When two keys hash to the same slot, the situation is called a collions
+    # A "random"-looking hash function can minimize the number of collions
     
