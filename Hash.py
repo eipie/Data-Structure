537abc75c70825700aa42dd521df44630cab84a6
Henry Ma <henrito3.cn@gmail.com>
2020-08-09 18:44:06 +0800

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -1,1 +1,45 @@
-# Hash
+# Hash Table Implementation
+# Implementation 1: using object-oriented programming
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def set(self, newNode):
+        self.next = newNode
+
+    def get(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    
+class Hash:
+    def __init__(self, slots):
+        self.table = [None] * slots
+        self.slots = slots 
+
+    def insert(self, data):
+        i = data % slots
+        newNode = Node(data)
+        if self.table[i] is None:
+            self.table[i] = newNode
+            return True
+        else:
+            head = self.table[i]
+            while head.getNext() is not None:
+                head = head.getNext()
+            head.set_next(newNode)
+            return True 
+
+    def search(self):
+        return
+
+    def delete(self):
+        return 
+    
+
+
+        
