90ea3a216b7f9d6e17b333e8af64fda95cdba1ae
Henry Ma <henrito3.cn@gmail.com>
2020-08-31 11:10:22 +0800

    Create AVL.py

---
+++ AVL.py
@@ -0,0 +1,34 @@
+# AVL Tree
+# 1. Balanced 
+#     - Binary search trees support insert, delete, min, max, next_Large and next_Smaller
+#     in O(h) time, where h is the height of the tree 
+#     - h is between log(n) and n
+#     - Balanced BST maintains h = O(log(n)) => all operations run at O(log(n)) time
+
+# 2. AVL trees
+#     - for every node, require heights of left and right children to differ by at most +- 1
+#     - treat nill tree as height -1
+#     - each node stores its height
+
+import Graphs
+import random
+import math
+
+class AVL(self):
+    def __init__(self):
+        return
+
+    def leftRotate(self):
+        return
+
+    def rightRotate(self):
+        return
+
+    def rebalance(self):
+        return
+
+    def insert(self):
+        return
+
+    def delete(self):
+        return 
